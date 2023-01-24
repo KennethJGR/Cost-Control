e210dd5762c654bf17595922adae146e6016f920
Kenneth Gaitan <gaitan.kj@gmail.com>
2023-01-24 14:47:12 -0600

    creating modal component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import IconNewSpent from "./img/nuevo-gasto.svg";
+import Modal from "./components/Modal";
+
+
+
 
 function App() {
   const [budget, setBudget] = useState(0);
   const [isValidBudget, setIsValidBudget] = useState(false);
+  const [modal, setModal] = useState(false);
+
+  const handleNewSpent = () => {
+    setModal(true);
+  };
 
   return (
     <div>
@@ -13,6 +23,14 @@ function App() {
         isValidBudget={isValidBudget}
         setIsValidBudget={setIsValidBudget}
       />
+
+      {isValidBudget && (
+        <div className="nuevo-gasto">
+          <img src={IconNewSpent} alt="IconNewSpent" onClick={handleNewSpent} />
+        </div>
+      )}
+
+      {modal && <p>"Desdemoal"</p>}
     </div>
   );
 }
