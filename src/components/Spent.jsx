68a1baa211b4f3c90df5487738a62677407a53dc
Kenneth Gaitan <gaitan.kj@gmail.com>
2023-01-25 15:18:19 -0600

    adding more helpers

--- src/components/Spent.jsx
+++ src/components/Spent.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { formatDateTime } from "../helpers";
 
 const Spent = ({ spents }) => {
-    const { name, category, id, amount } = spents;
+    const { name, category, id, amount, date } = spents;
 
     return (
         <div className="gasto sombra">
             <div className="contenido-gasto">
+                {/** Imagen */}
                 <div className="descripcion-gasto">
                     <p className="categoria">{category}</p>
                     <p className="nombre-gasto">{name}</p>
+                    <p className="fecha-gasto">
+                        Date:
+                        <span>{formatDateTime(date)}</span>
+                    </p>
                 </div>
             </div>
+            <p className="cantidad-gasto">${amount}</p>
         </div>
     );
 };
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,3 +5,14 @@ export const idGenerator = () => {
 
     return random + date;
 };
+
+export const formatDateTime = (date) => {
+    const newDate = new Date(date);
+
+    const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    };
+    return newDate.toLocaleDateString("en-US", options);
+};
