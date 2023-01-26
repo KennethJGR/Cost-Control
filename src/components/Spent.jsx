815f788c95982972815ec19b485bf255ad683ff4
Kenneth Gaitan <gaitan.kj@gmail.com>
2023-01-26 14:13:36 -0600

    changes in dependencies

--- src/components/Spent.jsx
+++ src/components/Spent.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import {
+    LeadingActions,
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions,
+} from "react-swipeable-list";
 
-import{} from "react-swipeable"
+import style from "react-swipeable-list/dist/styles.css";
 
 import { formatDateTime } from "../helpers";
 import iconSavings from "../img/icono_ahorro.svg";
@@ -24,22 +31,37 @@ const dictionaryIcons = {
 const Spent = ({ spents }) => {
     const { name, category, id, amount, date } = spents;
 
-    return (
-        <div className="gasto sombra">
-            <div className="contenido-gasto">
-                <img src={dictionaryIcons[category]} alt={category} />
+    const leadingActions = () => {
+        console.log("leadingActions");
+    }
+    const trailingActions = () => {
+        console.log("trailingActions");
+    }
 
-                <div className="descripcion-gasto">
-                    <p className="categoria">{category}</p>
-                    <p className="nombre-gasto">{name}</p>
-                    <p className="fecha-gasto">
-                        Date:
-                        <span>{formatDateTime(date)}</span>
-                    </p>
+
+    return (
+        <SwipeableList>
+            <SwipeableListItem
+                leadingActions={leadingActions}
+                trailingActions={trailingActions}
+            >
+                <div className="gasto sombra">
+                    <div className="contenido-gasto">
+                        <img src={dictionaryIcons[category]} alt={category} />
+
+                        <div className="descripcion-gasto">
+                            <p className="categoria">{category}</p>
+                            <p className="nombre-gasto">{name}</p>
+                            <p className="fecha-gasto">
+                                Date:
+                                <span>{formatDateTime(date)}</span>
+                            </p>
+                        </div>
+                    </div>
+                    <p className="cantidad-gasto">${amount}</p>
                 </div>
-            </div>
-            <p className="cantidad-gasto">${amount}</p>
-        </div>
+            </SwipeableListItem>
+        </SwipeableList>
     );
 };
 
